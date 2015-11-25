6068cde34611cf94c6e558d74c60efb18709bc05
Haoli Qu <haoli.qu@arm.com>
2015-11-25 16:29:34 +0000

    fixed the stretching body

--- js/settings.js
+++ js/settings.js
@@ -238,3 +238,17 @@
 	})
 
 })(jQuery);
+
+/* Detecting which sections are stretching the body container */
+/*
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
+*/
